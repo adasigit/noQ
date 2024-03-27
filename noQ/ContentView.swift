22bf80eb150eae54b7ce80d24e863154b015f4ae
Sigit Kurniawan <skurniawan24@bsd.idserve.net>
2024-03-27 10:08:30 +0700

    Initial Commit

---
+++ noQ/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  noQ
+//
+//  Created by Sigit Academy on 26/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+                    HStack{
+                        VStack(spacing: 5.0){
+                            Text("Store Location").font(.custom("Poppins-Regular", size: 12))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text("Lawson Syahdan")
+                                .font(.custom("Poppins-Regular", size: 16))
+                                .fontWeight(.bold)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Image("")
+                        }
+                    }
+                    
+                    // TODO: your content goes here...
+                    Text("content...")
+                    Spacer() /// forces content to top
+                }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ noQ/noQApp.swift
@@ -0,0 +1,17 @@
+//
+//  noQApp.swift
+//  noQ
+//
+//  Created by Sigit Academy on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct noQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
